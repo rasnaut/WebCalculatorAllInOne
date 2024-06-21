c2927c43056418b7fbfa9aa62263ffe87a807d09
Serhii Horb <serhii.horb.ua@gmail.com>
2024-06-21 12:28:52 +0200

    Add multiply

--- java_backend/j_calculator/src/main/java/com/example/j_calculator/CalculatorController.java
+++ java_backend/j_calculator/src/main/java/com/example/j_calculator/CalculatorController.java
@@ -5,9 +5,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/j_calculator")
 public class CalculatorController {
-    
+
     @GetMapping("/add")
     public String add(@RequestParam int a, @RequestParam int b) {
         return String.valueOf(a + b);
+    }
+
+    @GetMapping("/multiply")
+    public String multiply(@RequestParam int a, @RequestParam int b) {
+        return String.valueOf(a * b);
     }
 }
