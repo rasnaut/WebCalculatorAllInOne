e4575ad60d7dc730545cbdfbd40cc2e26a62137c
Ziabko <ziabko.kaplan@gmail.com>
2025-01-21 12:52:18 +0100

    autoreset

--- java_backend/j_calculator/src/main/java/com/example/j_calculator/CalculatorController.java
+++ java_backend/j_calculator/src/main/java/com/example/j_calculator/CalculatorController.java
@@ -11,11 +11,26 @@ public class CalculatorController {
         return String.valueOf(a + b);
     }
 
+    @GetMapping("/multiply")
+    public String multiply(@RequestParam int a, @RequestParam int b) {
+        return String.valueOf(a * b);
+    }
+
+    @GetMapping("/devide")
+    public String devide(@RequestParam int a, @RequestParam int b) {
+        return String.valueOf(a / b);
+    }
+
+    @GetMapping("/subtract")
+    public String substruct(@RequestParam int a, @RequestParam int b) {
+        return String.valueOf(a - b);
+    }
+
     @PostMapping("/sqrt")
-    public double sqrt (@RequestParam double a) {
+    public String  sqrt (@RequestParam double a) {
         if (a <= 0){
             throw new IllegalArgumentException("number has to be positive");
         }
-        return Math.sqrt(a);
+        return String.valueOf(Math.sqrt(a));
     }
 }
