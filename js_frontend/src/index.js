36f5d2f0d210f8e1f2e7ec26e8f7dfa40a021515
GalaktionovaOlga <galaktionovaolga555@gmail.com>
2025-06-16 13:21:15 +0300

    commit

--- js_frontend/src/index.js
+++ js_frontend/src/index.js
@@ -25,6 +25,15 @@ function calculate() {
                 return result;
              });
     }
+
+     if (currentOperation === '-') {
+        fetch(`http://localhost:8080/api/calculator/sub?a=${firstOperand}&b=${secondOperand}`)
+            .then(response => response.text())
+            .then(result => { 
+                display.value = result;
+                return result;
+             });
+    }
     return Promise.resolve();
 }
 
