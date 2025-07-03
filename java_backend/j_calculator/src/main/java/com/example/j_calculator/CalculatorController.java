a608d538048e66364cb2eaa476a394ab25e8d0ce
Mariia Shirokova <shirokova.maria14@gmail.com>
2025-07-03 14:34:06 +0200

    add log functionality

--- java_backend/j_calculator/src/main/java/com/example/j_calculator/CalculatorController.java
+++ java_backend/j_calculator/src/main/java/com/example/j_calculator/CalculatorController.java
@@ -3,26 +3,27 @@ package com.example.j_calculator;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/j_calculator")
+@RequestMapping("/api/calculator")
 public class CalculatorController {
-
-    private static final Logger logger = LoggerFactory.getLogger(CalculatorController.class);
 
     @GetMapping("/add")
     public String add(@RequestParam int a, @RequestParam int b) {
-        int result = a + b;
-        logger.info("Received request to add: {} + {} = {}", a, b, result);
-        return String.valueOf(result);
+        return String.valueOf(a + b);
     }
 
     @PostMapping("/sqrt")
     public String sqrt(@RequestParam double a) {
         if (a <= 0) {
-            logger.warn("Invalid input for sqrt: {}", a);
             throw new IllegalArgumentException("number has to be positive");
         }
-        double result = Math.sqrt(a);
-        logger.info("Received request to sqrt: sqrt({}) = {}", a, result);
-        return String.valueOf(result);
+        return String.valueOf(Math.sqrt(a));
+    }
+
+    @PostMapping("/log")
+    public String log(@RequestParam double a) {
+        if (a <= 0) {
+            throw new IllegalArgumentException("number has to be positive");
+        }
+        return String.valueOf(Math.log10(a));
     }
 }
