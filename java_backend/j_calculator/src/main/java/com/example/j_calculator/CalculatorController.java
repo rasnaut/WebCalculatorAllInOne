73537f50233b095b2f3f3fe65581dfe45575d255
Mariia Shirokova <shirokova.maria14@gmail.com>
2025-07-03 14:08:57 +0200

    add log functionality

--- java_backend/j_calculator/src/main/java/com/example/j_calculator/CalculatorController.java
+++ java_backend/j_calculator/src/main/java/com/example/j_calculator/CalculatorController.java
@@ -5,17 +5,24 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/j_calculator")
 public class CalculatorController {
-    
+
+    private static final Logger logger = LoggerFactory.getLogger(CalculatorController.class);
+
     @GetMapping("/add")
     public String add(@RequestParam int a, @RequestParam int b) {
-        return String.valueOf(a + b);
+        int result = a + b;
+        logger.info("Received request to add: {} + {} = {}", a, b, result);
+        return String.valueOf(result);
     }
 
     @PostMapping("/sqrt")
-    public String  sqrt (@RequestParam double a) {
-        if (a <= 0){
+    public String sqrt(@RequestParam double a) {
+        if (a <= 0) {
+            logger.warn("Invalid input for sqrt: {}", a);
             throw new IllegalArgumentException("number has to be positive");
         }
-        return String.valueOf(Math.sqrt(a));
+        double result = Math.sqrt(a);
+        logger.info("Received request to sqrt: sqrt({}) = {}", a, result);
+        return String.valueOf(result);
     }
 }
