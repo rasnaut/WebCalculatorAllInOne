17ecbc85bd32dfd4edcc982ddedd00827b8f8b0a
LiudmylaIermolenko <lussi.yer@gmail.com>
2025-07-03 14:21:31 +0200

    power 2 # 40

--- java_backend/j_calculator/src/main/java/com/example/j_calculator/CalculatorController.java
+++ java_backend/j_calculator/src/main/java/com/example/j_calculator/CalculatorController.java
@@ -19,7 +19,7 @@ public class CalculatorController {
         return String.valueOf(Math.sqrt(a));
     }
 
-    @PostMapping("/square")
+    @PostMapping("/pow2")
     public String square(@RequestParam double a) {
         return String.valueOf(Math.pow(a, 2));
     }
